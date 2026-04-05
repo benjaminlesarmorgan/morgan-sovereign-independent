e0f3965b63138213985d28d1cdbe76324f3e8a5b
Peter Steinberger <steipete@gmail.com>
2026-04-05 21:37:28 +0100

    fix: restore mattermost bundled entry root

--- extensions/mattermost/index.ts
+++ extensions/mattermost/index.ts
@@ -18,7 +18,7 @@ export default defineBundledChannelEntry({
   description: "Mattermost channel plugin",
   importMetaUrl: import.meta.url,
   plugin: {
-    specifier: "./src/channel.js",
+    specifier: "./entry-api.js",
     exportName: "mattermostPlugin",
   },
   runtime: {
